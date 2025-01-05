8c3f2094918613a3124e00d5ef8a6c31ede6f983
Alp Eray Taşçı <152800291+alpperay@users.noreply.github.com>
2025-01-05 18:08:19 +0300

    Hesap-Makinesi
    Switch- Case  yapısını kullanarak verilen rakama göre işlem yapan hesap makinesi projesi.

---
+++ Hesap-Makinesi/Hesap-Makinesi/HesapMakinesi.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hesap_Makinesi
+{
+    internal class HesapMakinesi
+    {
+        public void Topla()
+        {
+            int toplam;
+            Console.WriteLine("Birinci sayıyı giriniz :");
+            int sayi1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("İkinci sayıyı giriniz :");
+            int sayi2 = Convert.ToInt16(Console.ReadLine());
+            toplam = sayi1 + sayi2;
+            Console.WriteLine("Sayıların Toplamı :" + toplam);
+        }
+
+        public void Cikar()
+        {
+            int cikarma;
+            Console.WriteLine("Birinci sayıyı giriniz :");
+            int sayi1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("İkinci sayıyı giriniz :");
+            int sayi2 = Convert.ToInt16(Console.ReadLine());
+            if (sayi1 > sayi2)
+            {
+                cikarma = sayi1 - sayi2;
+                Console.WriteLine("Sonuç :" + cikarma);
+            }
+            else if (sayi1 < sayi2)
+            {
+                cikarma = sayi1 - sayi2;
+                Console.WriteLine("Sonuç :" + cikarma);
+            }
+            else
+            {
+                Console.WriteLine("Hatalı İşlem!");
+            } 
+        }
+
+        public void Carp()
+        {
+            int carp;
+            Console.WriteLine("Birinci sayıyı giriniz :");
+            int sayi1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("İkinci sayıyı giriniz :");
+            int sayi2 = Convert.ToInt16(Console.ReadLine());
+            carp = sayi1 * sayi2;
+            Console.WriteLine("Sonuç :" + carp);
+        }
+
+        public void Bol()
+        {
+            float bol;
+            Console.WriteLine("Bölünecek sayıyı Giriniz :");
+            float sayi1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Bölen sayıyı Giriniz :");
+            int sayi2 = Convert.ToInt16(Console.ReadLine());
+            if (sayi1 > sayi2)
+            {
+                bol = sayi1 / sayi2;
+                Console.WriteLine("Sayı :" + bol);
+            }
+            else if (sayi2 > sayi1)
+            {
+                bol = sayi1 / sayi2;
+                Console.WriteLine("Sayı :" + bol);
+            }
+            else if (sayi2 == 0)
+            {
+                Console.WriteLine("0'a bölme hatası!");
+            }
+            else
+            {
+                Console.WriteLine("Hatalı İşlem");
+            }
+        }
+    }
+}
---
+++ Hesap-Makinesi/Hesap-Makinesi/Program.cs
@@ -0,0 +1,41 @@
+﻿using Hesap_Makinesi;
+
+HesapMakinesi hm = new HesapMakinesi();
+string secim;
+do
+{
+    Console.WriteLine("Toplama İşlemi İçin ... 1");
+    Console.WriteLine("Çıkarma İşlemi İçin ... 2");
+    Console.WriteLine("Çarpma İşlemi İçin ... 3");
+    Console.WriteLine("Bölme İşlemi İçin ... 4");
+    Console.WriteLine("Çıkmak İçin ... 5");
+    secim = Console.ReadLine();
+    Console.Clear();
+
+    switch (secim)
+    {
+        case "1":
+            hm.Topla();
+            break;
+        case "2":
+            hm.Cikar();
+            break;
+        case "3":
+            hm.Carp();
+            break;
+        case "4":
+            hm.Bol();
+            break;
+        case "5":
+            Console.WriteLine("Çıkılıyor...");
+            break;
+        default:
+            Console.WriteLine("Geçersiz seçenek! Lütfen tekrar deneyin.");
+            break;
+    }
+    if (secim != "5")
+    {
+        Console.WriteLine("Devam etmek için bir tuşa basın...");
+        Console.ReadKey();
+    }
+} while (secim != "5");
